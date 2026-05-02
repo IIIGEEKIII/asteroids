dc14a430dfcf068b7f76b2ee2f7fea221b86719e
IIIGEEKIII <dillon_lippold@icloud.com>
2026-05-02 12:26:36 -0500

    Asteroid field spawn, no collision

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+from constants import *
+from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+def main():
+	pygame.init()
+	fps = pygame.time.Clock()
+	dt = 0
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	field = AsteroidField()
+
+	while True:
+		log_state()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill("black")
+		for drawing in drawable:
+			drawing.draw(screen)
+		pygame.display.flip()
+		fps.tick(60)
+		dt = fps.tick(60) / 1000
+		updatable.update(dt)
+
+if __name__ == "__main__":
+    main()
